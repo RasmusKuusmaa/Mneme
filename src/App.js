41f97696876881934509fcc2d7dd3704b2b997e6
77f315 <77f315@gmail.com>
2024-08-12 16:35:09 +0300

    Question generation and Hide text when answering

--- src/App.js
+++ src/App.js
@@ -2,6 +2,17 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  
+  const [question, setQuestion] = useState();
+  const [questionLength, setQuestionLength] = useState(6);
+  
+
+  const generateQuestion = () => {
+    setQuestion(Math.floor(Math.random() * 10 ** questionLength) + 10 ** questionLength )
+  }
+  
+
   //timer
   const [started, setStarted] = useState(false);
   const [isRunning, setIsRunning] = useState(false);
@@ -13,7 +24,7 @@ function App() {
       let IntervalId;
       IntervalId = setInterval(() => {
         setTime(prevTime => prevTime + 1)
-      }, 1);
+      }, 10);
       return () => clearInterval(IntervalId)
     }
    
@@ -21,16 +32,35 @@ function App() {
   const handleStart = () => {
     setStarted(true);
     setIsRunning(true);
+    generateQuestion();
+    setTime(0);
+  }
+  
+  const [hidden, setHidden] = useState(false);
+
+  const handleHide = () => {
+    setHidden(true);
+  }
+  //submitting
+  const handleSubmit = () => {
+    setIsRunning(false);
+    setHidden(false)
+    setStarted(false);
+      
   }
   return (
     <div>
       <div className='Settings'></div>
       <div className='Stats'></div>
-      <div className='QuestionBar'></div>
+      <div className='QuestionBar'>
+        <h1>{!hidden && question}</h1>
+      </div>
       <div className='AnswerBox'></div>
       <div className='Buttons'>
-        <button onClick={handleStart}>Start</button>
-        <h1>{time}</h1>
+        {!started && <button onClick={handleStart}>Start</button>}
+        {started && !hidden && <button onClick={handleHide}> Ready to answer</button>}
+        {started && hidden && <button onClick={handleSubmit}> Submit </button>}
+        <h1>{(time / 100).toFixed(2)}</h1>
       </div>
     </div>
   );
