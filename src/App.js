8e3eb0fecba94da2abfad1f434703f3acc88601f
77f315 <77f315@gmail.com>
2024-08-12 16:09:05 +0300

    added a timer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  //timer
+  const [started, setStarted] = useState(false);
+  const [isRunning, setIsRunning] = useState(false);
+  const [time, setTime] = useState(0);
+  
+  useEffect(() => {
+    if (isRunning) {
+
+      let IntervalId;
+      IntervalId = setInterval(() => {
+        setTime(prevTime => prevTime + 1)
+      }, 1);
+      return () => clearInterval(IntervalId)
+    }
+   
+  })
+  const handleStart = () => {
+    setStarted(true);
+    setIsRunning(true);
+  }
   return (
     <div>
       <div className='Settings'></div>
       <div className='Stats'></div>
       <div className='QuestionBar'></div>
       <div className='AnswerBox'></div>
-      <div className='Buttons'></div>
+      <div className='Buttons'>
+        <button onClick={handleStart}>Start</button>
+        <h1>{time}</h1>
+      </div>
     </div>
   );
 }
